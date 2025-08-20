802a007a752b4d9508190c022882dab4ce2a7506
Saksham <sakshambeniwal123@gmail.com>
2025-08-21 02:45:51 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { Login } from './Redux/slices/Log_status'
-import AuthServices from './Services/auth'  // <-- your Appwrite service
+// <-- your Appwrite service
 import './App.css'
 
 const App: React.FC = () => {
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    const restoreSession = async () => {
-      try {
-        // Always check Appwrite first to verify if session is still valid
-        const userdata = await AuthServices.Current_User()
-        if (userdata && !userdata?.code) {
-          // Valid session confirmed by Appwrite, store and dispatch login
-          localStorage.setItem('user', JSON.stringify(userdata))
-          dispatch(Login({ data: userdata }))
-        } else {
-          // No valid session from Appwrite, no dispatch
-          console.log('No valid user session found')
-        }
-      } catch (err) {
-        console.error('Session restore failed:', err)
-      }
-    }
-
-    restoreSession()
-  }, [dispatch])
 
   return (
     <div>
